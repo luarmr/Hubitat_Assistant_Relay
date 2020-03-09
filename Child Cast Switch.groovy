2441daa0b4a0f1396e314d82bbf0c1b748264601
ryancasler <ryancasler@users.noreply.github.com>
2020-03-09 16:18:41 -0400

    Initial Commit
    Initial commit of both files.

---
+++ Child Cast Switch.groovy
@@ -0,0 +1,129 @@
+/*
+ *  Google Home Assistant Relay v3
+ *
+ *  Copyright 2020 Ryan P. Casler
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *  
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2020-03-09  Ryan Casler    Initial Release
+ *
+ */
+metadata {
+    definition(name: "Child Cast Switch", namespace: "ryan780", author: "Ryan780") {
+        capability "Actuator"
+        capability "Switch"
+        command "toggle"
+        command "castWebsite", [[name:"Website*", type: "STRING", description: "Enter the website to cast."]]
+        command "castMedia", [[name:"Media Path or URL*", type: "STRING", description: "Enter the path or URL for the media to be cast."]]
+    }
+    preferences{
+        input "defaultType", "enum", title: "Cast Type", required: true, options:[["website":"Website"],["local":"Local Media"],["remote":"Remote Media"]]
+        input "defaultContent", "string", title: "Website URL or media path", required: true
+        input "castDevice", "string", title: "Device name", required: true
+        input "maxOn", "number", title: "Max On time in seconds", required: false, description:"Enter the Max On time, if desired"
+        input name: "enableMute", type: "bool", title: "Enable mute/unmute", defaultValue: true
+        input name: "enableStop", type: "bool", title: "Enable Stop after timeout", defaultValue: true
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    }
+}
+
+def installed(){
+    updated()
+}
+
+def logsOff() {
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable", [value: "false", type: "bool"])
+}
+
+def updated() {
+    log.info "updated..."
+    log.warn "debug logging is: ${logEnable == true}"
+    if (logEnable){
+        runIn(1800, logsOff)
+    }else{
+        unschedule(logsOff)
+    }
+    if(settings.maxOn){
+        state.maxOn = settings.maxOn.toInteger()
+    }else{
+        state.maxOn = false
+    }
+}
+
+
+def parse(String description) {
+    if (logEnable) log.debug(description)
+}
+
+def on() {
+    if (logEnable) log.debug "Switching ON.  Casting ${defaultType} ${defaultContent}"
+    if(device.currentValue("switch") !="on"){
+        if(enableMute){
+            parent.mute(castDevice)
+        }
+        parent.castContent(castDevice, defaultType, defaultContent)
+    }
+    if(state.maxOn){
+        unschedule(off)
+        runIn (state.maxOn, off)
+    }
+    sendEvent(name:"switch", value:"on")
+}
+
+def off() {
+    unschedule(off)
+    if (logEnable) log.debug "OFF called"
+    if(enableStop){
+        parent.stop(castDevice)
+    }
+    if (enableMute){
+        parent.unmute(castDevice)
+    }
+    sendEvent(name: "switch", value:"off")
+}
+
+def toggle(){
+    if(device.currentValue("switch")=="on"){
+        off()
+    }else{
+        on()
+    }
+}
+
+
+def castWebsite(website){
+    if (logEnable) log.debug "Casting website ${website}"
+    if(device.currentValue("switch") !="on"){
+        if (enableMute){
+            parent.mute(castDevice)
+        }
+        parent.castContent(castDevice, "website", website)
+        if(state.maxOn){
+            runIn (state.maxOn, off)
+        }
+        sendEvent(name:"switch", value:"on")
+    }
+}
+
+def castMedia(media){
+    if (logEnable) log.debug "Casting meida ${media}"
+    if(device.currentValue("switch") !="on"){
+        if (enableMute){
+            parent.mute(castDevice)
+        }
+        parent.castContent(castDevice, "remote", media)
+        if(state.maxOn){
+            runIn (state.maxOn, off)
+        }
+        sendEvent(name:"switch", value:"on")
+    }
+}
---
+++ Google Assistant Relay v3.groovy
@@ -0,0 +1,247 @@
+/*
+ *  Google Home Assistant Relay v3
+ *
+ *  Copyright 2020 Ryan Casler
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *  
+ *  Change History:
+ *
+ *    Date        Who            What
+ *    ----        ---            ----
+ *    2020-03-09  Ryan Casler    Initial Release
+ *
+ *
+ *  Based on the Google Assistnt Relay driver from Daniel Ogorchock 
+ *
+ *  Orignal Software License:
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *    Credit goes to Greg Hesp's work on the SmartThings platform as a starting point for this version!
+ */
+
+metadata {
+    definition (name: "Google Home Assistant Relay v3", namespace: "ryan780", author: "Ryan780") {
+        capability "Speech Synthesis"
+        capability "Notification"
+        command "newChildDevice", [[name: "New Child Device Name*", type: "STRING", description: "Enter the name of the new child device"]]
+        command "mute", [[name:"Cast Device", type: "STRING", description: "If nothing is entered, the default device will be used."]]
+        command "unmute", [[name:"Cast Device", type: "STRING", description: "If nothing is entered, the default device will be used."]]
+        command "castContent", [[name:"Cast Device",type: "STRING", description: "If nothing is entered, the default device will be used."],
+                                [name: "Cast Type*",type: "ENUM", constraints:["website","local","remote"], description: "Content type"],
+                                [name:"Source*", type: "STRING", description: "Cast sourse"]]
+        command "stop", [[name:"Cast Device", type: "STRING", description: "If nothing is entered, the default device will be used."]]
+    }
+
+    preferences {
+        input(name: "serverIP", type: "string", title:"Server IP Address", description: "Enter IP Address of your Assistant Relay Server", required: true, displayDuringSetup: true)
+        input(name: "serverPort", type: "string", title:"Server Port", description: "Enter Port of your Assistant Relay Server (defaults to 3000)", defaultValue: "3000", required: true, displayDuringSetup: true)
+        input(name: "user", type: "string", title:"Assistant Relay Username", description: "Enter the username for this device", defaultValue: "", required: false, displayDuringSetup: true)
+        input(name: "defaultDevice", type: "string", title:"Display Device IP or Name", description: "Enter the IP address or name of your dispplay device", required: true, displayDuringSetup: true)
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    }
+}
+
+def installed() {
+    updated()
+}
+
+def updated() {
+    if (logEnable){
+        runIn(1800,logsOff)
+    }else{
+        unschedule(logsOff)
+    }
+}
+
+def initialialized(){
+    updated()
+}
+
+def logsOff(){
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
+}
+
+def parse(String description) {
+    //if (logEnable) log.debug "Parsing ${description}"
+}
+
+def speak(message) {
+    message = message.replaceAll("%20", " ")
+    message = message.replace("Å", "\\u00C5")
+    message = message.replace("å", "\\u00E5")
+    message = message.replace("Ä", "\\u00C4")
+    message = message.replace("ä", "\\u00E4")
+    message = message.replace("Ö", "\\u00D6")
+    message = message.replace("ö", "\\u00F6")
+    message = message.replace("Ø", "\\u00D8")
+    message = message.replace("Æ", "\\u00C6")
+    message = message.replace("æ", "\\u00E6")
+    message = message.replace("ø", "\\u00F8")
+    def myJSON = ""
+    if(message.startsWith("[CC]")){ 
+        message = message.minus("[CC]")
+        if (user) {
+            myJSON = "{ \"command\": \"${message}\",\"user\": \"${user}\" }"
+        } else {
+            myJSON = "{ \"command\": \"${message}\" }"
+        }
+    }  
+    else if(message.startsWith("[CCC]")){ 
+        message = message.minus("[CCC]")
+        if (user) {
+            myJSON = "{ \"command\": \"${message}\",\"user\": \"${user}\",\"converse\": \"true\" }"
+        } else {
+            myJSON = "{ \"command\": \"${message}\",\"converse\": \"true\" }"
+        }
+    } 
+    else if(message.startsWith("[P]")){ 
+        message = message.minus("[P]")
+        if (user) {
+            myJSON = "{ \"preset\": \"${message}\",\"user\": \"${user}\" }"
+        } else {
+            myJSON = "{ \"preset\": \"${message}\" }"
+        }
+    } 
+    else {
+        if (user) {
+            myJSON = "{ \"command\": \"${message}\",\"user\": \"${user}\",\"broadcast\": \"true\" }"
+        } else {
+            myJSON = "{ \"command\": \"${message}\",\"broadcast\": \"true\" }"
+        }
+    }
+    httpPostJSON(myJSON)  
+}
+
+def deviceNotification(message) {
+    speak(message)
+}
+
+def httpPostJSON(myJSON) {
+    try {
+        if (logEnable) log.debug "Sending ${myJSON} to ${serverIP}:${serverPort}"
+        def headers = [:]
+        headers.put("HOST", "${serverIP}:${serverPort}")
+        headers.put("Content-Type", "application/json")
+        def method = "POST"
+        def path = "/assistant"
+        def result = new hubitat.device.HubAction(
+            method: method,
+            path: path,
+            body: myJSON,
+            headers: headers
+        )
+        return result
+    } catch (Exception e) {
+        log.error "Error = ${e}"
+    } 
+}
+
+def newChildDevice(deviceName){
+    def newId = device.id + "-"+deviceName
+    log.info "Creating Child Device ${deviceName} with ID: ${newId})'"
+    try {            
+        addChildDevice("Child Cast Switch", "${newId}",[label: "${deviceName}", isComponent: false, name: "${device.displayName}-${deviceName}"])         
+    } catch (e) {
+        log.error "Child device creation failed with error = ${e}"
+    }
+}
+
+def mute(castDevice){
+    if(logEnable)log.debug "Muting ${castDevice}."
+    def message = "Mute ${castDevice}"
+    def myJSON = "{ \"command\": \"${message}\",\"user\": \"${user}\" }"
+    httpPostJSON(myJSON)
+}
+
+def mute(){
+    if(logEnable)log.debug "Muting default device."
+    mute(defaultDevice)
+}
+
+def unmute(castDevice){
+    if(logEnable)log.debug "Unmuting ${castDevice}."
+    def message = "Unmute ${castDevice}"
+    def myJSON = "{ \"command\": \"${message}\",\"user\": \"${user}\" }"
+    httpPostJSON(myJSON)
+}
+
+def unmute(){
+    if(logEnable)log.debug "Unmuting default device."
+    unmute(defaultDevice)
+}
+
+def castContent(castDevice, type, source){
+    if(logEnable)log.debug "Casting ${source} of ${type} to ${castDevice}" 
+    def myJSON = "{ \"type\": \"${type}\",\"source\": \"${source}\",\"device\": \"${castDevice}\" }"
+    try {
+        if (logEnable) log.debug "Sending ${myJSON} to ${serverIP}:${serverPort}"
+        def headers = [:]
+        headers.put("HOST", "${serverIP}:${serverPort}")
+        headers.put("Content-Type", "application/json")
+        def method = "POST"
+        def path = "/cast"
+        def result = new hubitat.device.HubAction(
+            method: method,
+            path: path,
+            body: myJSON,
+            headers: headers
+        )
+        return result
+    } catch (Exception e) {
+        log.error "Error = ${e}"
+    } 
+}
+
+def castContent(type, source){
+    castContent(defaultDevice, type, source)
+}
+
+def stop(String castDevice){
+    if(logEnable)log.debug "Stopping ${castDevice}"
+    def myJSON = "{ \"device\": \"${castDevice}\" }"
+    try {
+        if (logEnable) log.debug "Sending ${myJSON} to ${serverIP}:${serverPort}"
+        def headers = [:]
+        headers.put("HOST", "${serverIP}:${serverPort}")
+        headers.put("Content-Type", "application/json")
+        def method = "POST"
+        def path = "/cast/stop"
+        def result = new hubitat.device.HubAction(
+            method: method,
+            path: path,
+            body: myJSON,
+            headers: headers
+        )
+        return result
+    } catch (Exception e) {
+        log.error "Error = ${e}"
+    } 
+}
+
+def stop(){
+    def castDevice = defaultDevice
+    stop(castDevice)
+}
+
+def uninstalled(){
+    children = getChildDevices()
+    children.each{it->
+        deleteChildDevice(it.deviceNetworkId)
+    }
+    log.debug "All children devices removed."
+}
